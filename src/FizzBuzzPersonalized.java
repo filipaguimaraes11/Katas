3b441a97fb36d920ce9c75c4238014710a901215
FilipaGuimaraes <filipaguimaraes.11@gmail.com>
2022-11-18 10:48:03 +0000

    Changed personalized

--- src/FizzBuzzPersonalized.java
+++ src/FizzBuzzPersonalized.java
@@ -1,27 +1,23 @@
-public class FizzBuzzPersonalized {
+import java.util.List;
 
+public class FizzBuzzPersonalized{
+
+    private List<String> data;
+
+    public FizzBuzzPersonalized(List<String> arr){
+        this.data = arr;
+    }
     //multiplication chosen by player
-    private String multipleChosenByPlayer = String.
+    //private String multipleChosenByPlayer = String.
 
     //words chosen by player
-    private String[] playerOptions = [multiplierOne, multiplierTwo, fizzWord, buzzWord];
+    //private String[] playerOptions = [multiplierOne, multiplierTwo, fizzWord, buzzWord];
 
 
-    public String playBasicGame(int num ) {
-        if (num % 3 == 0 && num % 5 == 0) {
-            System.out.println("FizzBuzz");
-            return "FizzBuzz";
-        }
-        else if (num % 3 == 0) {
-            System.out.println("Fizz");
-            return "Fizz";
-        }
-        else if (num % 5 == 0) {
-            System.out.println("Buzz");
-            return "Buzz";
-        }
-        System.out.println("" + num);
-        return ""+num;
+    for( String value : data){
+
+        
+
     }
 
 }
